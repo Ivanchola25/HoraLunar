4a6303805b17aad2646f06329ed911b0501d9263
Ivanchola25 <ivanlatorre2512@hotmail.com>
2025-11-05 21:37:39 -0500

    Add files via upload

---
+++ Js/AnimTexto.js
@@ -0,0 +1,16 @@
+const paragraphs = document.querySelectorAll(".TextAnim");
+document.addEventListener("scroll", function(){
+    paragraphs.forEach((paragraph) =>{
+        if(isInView(paragraph)){
+            paragraph.classList.add("TextAnim--visible");
+        }
+    });
+});
+function isInView(element){
+    const rect = element.getBoundingClientRect();
+    return(
+        rect.bottom > 0 &&
+        rect.top <
+        (window.innerHeight -150 || document.documentElement.clientHeight - 150)
+    );
+}
